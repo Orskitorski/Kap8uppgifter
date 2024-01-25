863ab16719ebaab3c2fd7375bce1a70e71e9722c
Orskitorski <oscar.tafvelin@elev.ga.ntig.se>
2024-01-25 10:39:27 +0100

    la till kommentarer som förklarar stegen

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Calendar;
 
 public class Main {
     public static void main(String[] arg) {
-        System.out.println(upg87());
+        System.out.println(upg87()); //Kallar på metoden och skriver ut metodens return value i konsolen
         System.out.println(upg88());
     }
 
@@ -14,15 +14,16 @@ public class Main {
     public static String upg88() {
         String date = Calendar.getInstance().getTime().toString();
         String[] montharray = new String[]{("Jan"), ("Feb"), ("Mar"), ("Apr"), ("May"), ("Jun"), ("Jul"), ("Aug"), ("Sep"), ("Oct"), ("Nov"), ("Dec")};
-        String[] datearray = date.split(" ");
+        String[] datearray = date.split(" "); //Splittar upp datumet till en array där alla strings separeras där det finns ett mellanslag
 
-        String month = null;
-        String day = datearray[2];
-        String year = datearray[5];
+        String month = null; //Initierar variabeln month som kommer användas i for loopen
+        String day = datearray[2]; //Sätter variabeln day till stringen på plats 2 i arrayen
+        String year = datearray[5]; //Sätter variabeln year till stringen på plats 5 i arrayen
 
+        //For loop för att gå igenom varje månad i arrayen steg för steg (i ökar varje gång loopen körs) och kolla om datumet innehåller den specifika stringen
         for (int i = 0; i<12; i++) {
             if (date.contains(montharray[i])) {
-                i++;
+                i++; //adderar 1 på i för att månaden ska bli rätt (array börjar alltid på plats 0)
                 if (i < 10) {
                     month = ("0"+ i);
                 }
